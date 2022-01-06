17f24345f7a1f38b3880bd8f0aa98ce0670e5f2f
Nick Smith <nick.smith@cern.ch>
2022-01-06 12:19:46 -0600

    Don't forget other MC

--- Fitting/topPurityFitting.py
+++ Fitting/topPurityFitting.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
+"""
+First source LCG environment to get ROOT:
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
+"""
 from ROOT import TFile, TFractionFitter, TObjArray
 
 import pprint
 
 _file = TFile("../RootFiles/M3_Output.root")
+
+def getSafe(name):
+    h = _file.Get(name)
+    if h == None:
+        print(h)
+        raise KeyError(name)
+    return h
 
 
 systematics  = ["nominal",
@@ -10,18 +22,18 @@ systematics  = ["nominal",
                 "FSRUp",
                 "ISRDown",
                 "ISRUp",
-                "JERDown",
-                "JERUp",
-                "JESDown",
-                "JESUp",
+                # "JERDown",
+                # "JERUp",
+                # "JESDown",
+                # "JESUp",
                 "PDFDown",
                 "PDFUp",
                 "Q2ScaleDown",
                 "Q2ScaleUp",
-                "btagWeight_heavyDown",
-                "btagWeight_heavyUp",
-                "btagWeight_lightDown",
-                "btagWeight_lightUp",
+                # "btagWeight_heavyDown",
+                # "btagWeight_heavyUp",
+                # "btagWeight_lightDown",
+                # "btagWeight_lightUp",
                 "eleEffWeightDown",
                 "eleEffWeightUp",
                 "muEffWeightDown",
@@ -32,13 +44,13 @@ systematics  = ["nominal",
 
 results = {}
 
-data = _file.Get("dataObs")
+data = getSafe("dataObs")
     
 for syst in systematics:
 
     mc = TObjArray(2)
-    mc.Add(_file.Get(f"TopPair_{syst}"))
-    mc.Add(_file.Get(f"NonTop_{syst}"))
+    mc.Add(getSafe(f"TopPair_{syst}"))
+    mc.Add(getSafe(f"NonTop_{syst}"))
 
     fit = TFractionFitter(data, mc,"q")
     
